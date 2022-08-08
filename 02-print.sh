a74c91c7a974121987f8d553cecf9a15b054d1ae
rajesh <rajesh.shahya@gmail.com>
2022-08-08 21:02:06 +0530

    message

---
+++ 02-print.sh
@@ -0,0 +1,10 @@
+#!/bin/bash 
+
+
+# echo -e "\e[COL-CODEm  MESSAGE  \e[0m" 
+
+echo -e "\e[33m I am printing YELLO COLOR \e[0m "
+echo -e "\e[31m I am printing RED COLOR \e[0m "
+echo -e "\e[32m I am printing GREEN COLOR \e[0m "
+echo -e "\e[34m I am printing BLUE COLOR \e[0m "
+echo -e "\e[35m I am printing MAGNETA COLOR \e[0m "
