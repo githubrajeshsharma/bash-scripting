6918d20d0c5afc0599a0520029bbb62bc2d6528f
rajesh <rajesh.shahya@gmail.com>
2022-08-08 21:46:15 +0530

    mesage

--- 04-vars.sh
+++ 04-vars.sh
@@ -1,5 +1,5 @@
 #!/bin/bash 
 
 DATE_COMMAND=$(date +%F) 
-echo "Good Morning, Todays date is $DATE_COMMAND"
+echo "Good Morning, Today's date is $DATE_COMMAND"
 echo "Total number of opened sessions are $(who |wc -l)"
