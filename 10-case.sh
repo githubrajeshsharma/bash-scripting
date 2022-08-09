0d15f7181e3b3cc446b0c28e8b5759872b4a0703
rajesh <rajesh.shahya@gmail.com>
2022-08-09 12:11:43 +0530

    mesage

---
+++ 10-case.sh
@@ -0,0 +1,27 @@
+#!/bin/bash 
+
+# case $var in 
+#     opt1)  commands ;; 
+#     opt2)  commands ;; 
+
+# esac 
+
+ACTION=$1
+
+case $ACTION in 
+    start)  
+        echo "XYZ Service Starting"
+        exit 0
+        ;;
+    stop)  
+        echo "XYZ Service Stopping"
+        exit 0
+        ;;
+    *)
+        echo -e "\e[33m Valid options are start or stop only \e[0m"
+        exit 1 
+esac
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
